398418f2e78c84942acb5cb6566ecc2353dfe66a
zm <Emails Notifications Billing SSH and GPG keys Security Sessions Blocked users Repositories Organizations Saved replies Applications Developer settings Emails email.manhao@gmail.com>
2019-02-15 13:31:15 +0800

    python

--- portscan.py
+++ portscan.py
@@ -1,13 +1,13 @@
 import nmap
 import csv
-import os
-def write_csv(info):
-    with open("test.csv", "a",newline='') as csvfile:
+import sys,getopt
+def write_csv(info,outfile):
+    with open(outfile, "a",newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(info)
-def scan_port(iplist):
+def scan_port(iplist,outfile):
     nm = nmap.PortScanner()
-    scaninfos = nm.scan(hosts=iplist, arguments='-n -Pn -sT -sV -p22,3306,445,80', sudo=False)
+    scaninfos = nm.scan(hosts=iplist, arguments='-n -Pn -sT -sV -p80', sudo=False)
     print(scaninfos)
     port_list = scaninfos['scan'][iplist]['tcp'].keys()
     print(port_list)
@@ -15,18 +15,43 @@ def scan_port(iplist):
         if (scaninfos['scan'][iplist]['tcp'][i]['state'] == 'open'):
             port_name = scaninfos['scan'][iplist]['tcp'][i]['name']
             version = scaninfos['scan'][iplist]['tcp'][i]['extrainfo'] if scaninfos['scan'][iplist]['tcp'][i]['extrainfo'] != '' else 'unknow version'
-            port_service = scaninfos['scan'][iplist]['tcp'][i]['product'] + '--'+version
+            product = scaninfos['scan'][iplist]['tcp'][i]['product']
+            port_service ='product: '+product+'     version:'+version
             info = [iplist, i, 'tcp', port_name, port_service]
             print(info)
-            write_csv(info)
-def read_iplist():
-    with open("1.txt",'r') as ip_list:
+            write_csv(info,outfile=outfile)
+def read_iplist(inputfile):
+    with open(inputfile,'r') as ip_list:
         lists = ip_list.read().splitlines()
         print(lists)
         return lists
-
-if __name__ == "__main__":
-    iplist=read_iplist()
+def help():
+    print("About information:python3 portscan.py -i file -o file [-选项 参数]")
+    print("-" * 60)
+    print("|  -o --output                 -将结果输出到file")
+    print("|  -t --thread                 -设置运行线程-default 4")
+    print("|  -i --input                  -输入存有ip列表")
+    print("-" * 60)
+    sys.exit(0)
+def start():
+    if not len(sys.argv[1:]):
+        help()
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "o:t:i:h", ["output", "thread", "input","help"])
+    except Exception as e:
+        print(e)
+        help()
+    for op, value in opts:
+        if op == "-i":
+            input_file = value
+        elif op == "-o":
+            output_file = value
+        elif op == "-h":
+            help()
+            sys.exit()
+    iplist = read_iplist(input_file)
     for ip in iplist:
         print(ip)
-        scan_port(ip)
+        scan_port(ip,output_file)
+if __name__ == "__main__":
+    start()
